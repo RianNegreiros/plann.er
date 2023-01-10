891be4d6119582119e97b9cf17bd5461ca5f4de9
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2023-01-10 17:34:25 -0300

    pages

--- clients/admin/src/App.tsx
+++ clients/admin/src/App.tsx
@@ -1,42 +1,19 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 import './App.css';
-import Menu from './components/Menu';
-import Nav from './components/Nav';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Users from './pages/Users';
 
 function App() {
   return (
     <div className="App">
-      <Nav />
-      <Menu />
-      <div className="container-fluid">
-        <div className="row">
-          <main className="col-md-9 ms-sm-auto col-lg-10 px-md-4">
-            <div className="table-responsive">
-              <table className="table table-striped table-sm">
-                <thead>
-                  <tr>
-                    <th scope="col">#</th>
-                    <th scope="col">Header</th>
-                    <th scope="col">Header</th>
-                    <th scope="col">Header</th>
-                    <th scope="col">Header</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <td>1,001</td>
-                    <td>random</td>
-                    <td>data</td>
-                    <td>placeholder</td>
-                    <td>text</td>
-                  </tr>
-                </tbody>
-              </table>
-            </div>
-          </main>
-        </div>
-      </div>
-      </div>
+      <BrowserRouter>
+          <Route path="/" component={Users} />
+          <Route path="/login" component={Login} />
+          <Route path="/register" component={Register} />
+      </BrowserRouter>
+    </div>
   );
 }
 
