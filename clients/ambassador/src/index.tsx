0a2f15b24a2dfdc6aac142afb05f6040b544be69
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2023-01-11 13:20:58 -0300

    config axios default and redux

--- clients/ambassador/src/index.tsx
+++ clients/ambassador/src/index.tsx
@@ -2,12 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import axios from 'axios';
+import { configureStore } from './redux/confiureStore';
+import { Provider } from 'react-redux';
+
+axios.defaults.baseURL = 'http://localhost:3000/api/ambassador';
+axios.defaults.withCredentials = true;
+
+const store = configureStore()
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
