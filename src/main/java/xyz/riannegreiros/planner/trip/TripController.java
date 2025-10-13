d7b1be9a6efb2644743fae9cfd4f7703a38d0c04
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2025-10-13 12:36:28 -0300

    feat: add get trip details endpoint

--- src/main/java/xyz/riannegreiros/planner/trip/TripController.java
+++ src/main/java/xyz/riannegreiros/planner/trip/TripController.java
@@ -1,11 +1,17 @@
 package xyz.riannegreiros.planner.trip;
 
+import java.util.Optional;
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
 
 @RestController
 @RequestMapping("/trips")
@@ -25,5 +31,12 @@ public class TripController {
     this.service.registerParticipantsToEvent(payload.emails_to_invite(), newTrip.getId());
 
     return ResponseEntity.ok(new TripCreateResponse(newTrip.getId()));
+  }
+
+  @GetMapping("/{id}")
+  public ResponseEntity<Trip> getTripDetails(@PathVariable UUID id) {
+      Optional<Trip> trip = this.repository.findById(id);
+
+      return trip.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
   }
 }
