ac6ea102a8215bb4421589ed5678f5d8e5199d21
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2025-10-14 07:34:51 -0300

    feat: add update trip details endpoint

--- src/main/java/xyz/riannegreiros/planner/trip/TripController.java
+++ src/main/java/xyz/riannegreiros/planner/trip/TripController.java
@@ -1,17 +1,13 @@
 package xyz.riannegreiros.planner.trip;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/trips")
@@ -39,4 +35,22 @@ public class TripController {
 
       return trip.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
   }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Trip> updateTripDetails(@PathVariable UUID id, @RequestBody TripRequestPayload payload) {
+        Optional<Trip> trip = this.repository.findById(id);
+        
+        if (trip.isPresent()) {
+            Trip tripUpdated = trip.get();
+            tripUpdated.setDestination(payload.destination());
+            tripUpdated.setStartsAt(LocalDateTime.parse(payload.starts_at(), DateTimeFormatter.ISO_DATE_TIME));
+            tripUpdated.setEndsAt(LocalDateTime.parse(payload.ends_at(), DateTimeFormatter.ISO_DATE_TIME));
+            
+            this.repository.save(tripUpdated);
+            
+            return ResponseEntity.ok(tripUpdated);
+        }
+
+        return ResponseEntity.notFound().build();
+    }
 }
